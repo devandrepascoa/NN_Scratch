f9bd05fb6539c8cfb65b265747c9aed214fd0893
André Páscoa <andre@pascoa.org>
2020-05-04 01:03:21 +0100

    Fixed shit

--- neural/optimizers.py
+++ neural/optimizers.py
@@ -10,7 +10,7 @@ def gradient_descent(self, gradients, parameters, learning_rate):
 
 
 # Function to adjust our weights and biases(parameters) based on the respective gradient and learning rate
-def gradient_descent_with_momentum(self, epoch, gradients, parameters, threshold=5.0, learning_rate):
+def gradient_descent_with_momentum(self, epoch, gradients, parameters, learning_rate, threshold=5.0):
     length = len(self.S)  # neural network length
     for i in range(1, length):
         self.directions["mdw" + str(i)] = self.Beta1 * self.directions["mdw" + str(i)] \
